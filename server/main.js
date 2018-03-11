a9f1b30b919cd202a7837654350aec51cdd51b6e
ticooo95 <ahmed1995bazzara@gmail.com>
2018-03-11 01:14:13 +0100

    using publish and subscribe between sever and client,, autopublish is dsiabled

--- server/main.js
+++ server/main.js
@@ -1,7 +1,10 @@
 import { Meteor } from 'meteor/meteor';
+import { Records } from '/imports/api/records.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
 });
 
-Records = new Mongo.Collection('records');
+Meteor.publish('records', function() {
+  return Records.find();
+})
